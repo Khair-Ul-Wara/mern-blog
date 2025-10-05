7833e3dbeb0c72efdbdb9f5da126145306d4e3a8
Khair Ul Wara <mynameiskhairulwara@gmail.com>
2025-10-05 13:59:58 +0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,59 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1GcV_nnQV0dzAPXbaMj5F8_2mUFkvYZyubw&s" alt="blog" />
+        </div>
+        <div className="texts">
+          <h2>My First Blog Post</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque aliquam odio et faucibus. Nulla rhoncus feugiat eros quis consectetur.</p>
+        </div>  
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1GcV_nnQV0dzAPXbaMj5F8_2mUFkvYZyubw&s" alt="blog" />
+        </div>
+        <div className="texts">
+          <h2>My First Blog Post</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque aliquam odio et faucibus. Nulla rhoncus feugiat eros quis consectetur.</p>
+        </div>  
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1GcV_nnQV0dzAPXbaMj5F8_2mUFkvYZyubw&s" alt="blog" />
+        </div>
+        <div className="texts">
+          <h2>My First Blog Post</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque aliquam odio et faucibus. Nulla rhoncus feugiat eros quis consectetur.</p>
+        </div>  
+      </div>
+    <div className="post">
+        <div className="image">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1GcV_nnQV0dzAPXbaMj5F8_2mUFkvYZyubw&s" alt="blog" />
+        </div>
+        <div className="texts">
+          <h2>My First Blog Post</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque aliquam odio et faucibus. Nulla rhoncus feugiat eros quis consectetur.</p>
+        </div>  
+      </div>
+
+
+      
+    </main>
+  );
+}
+
+export default App;
